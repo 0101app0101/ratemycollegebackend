421e759cca4808c471b0a6169513553660969563
akyp888 <akashrkashyap98@gmail.com>
2021-11-14 00:33:11 +0530

    added endpoints

--- routes/collegerouter.js
+++ routes/collegerouter.js
@@ -13,6 +13,17 @@ router.get('/', async (req,res) => {
    }
 })
 
+router.get('/:id', async (req,res) => {
+    try{
+ 
+     const colleges = await College.findById(req.params.id)
+     res.json(colleges).status(200)
+ 
+    }catch (e){
+     res.status(500).json({errormsg:e})
+    }
+ })
+
 router.post('/createcollege', async (req,res)=> {
 
 
@@ -44,6 +55,36 @@ router.post('/createcollege', async (req,res)=> {
             res.status(500).json({errormsg:e})
     }
 })
+
+router.put('/addreview',async (req,res)=>{
+
+    const updatedCollege = {
+
+        $push:{comments:req.body.comments},
+        ratings:req.body.ratings,
+        stars:{
+            star1:req.body.stars.star1,
+            star2:req.body.stars.star2,
+            star3:req.body.stars.star3,
+            star4:req.body.stars.star4
+        }
+    
+    } 
+
+        const updatecolleges = await College.findByIdAndUpdate(req.body.id,updatedCollege,
+            function (err, docs) {
+                if (err){
+                    res.status(500).json({errormsg:err})
+                }
+                else{
+                    res.json({updated:docs}).status(200)
+                    
+                    
+                    
+                }})
+
+  
+    })
 
 
 module.exports = router
